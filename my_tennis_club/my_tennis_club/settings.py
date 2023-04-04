e31d853d88f3bc91a8f26f5a069d7b7621b75efd
Nagu0804 <114297885+Nagu0804@users.noreply.github.com>
2023-04-04 16:23:16 +0530

    Add files via upload

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!4o8gaq*yd2yztd)^2!0hm8k(9ey_+wh_b+)r3xf4u&r@vxb^-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
